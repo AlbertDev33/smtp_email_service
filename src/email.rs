fd6e3c69b2e3a9e05f0b58c63650cf2197d896c4
Albert <albert.rocha@r2t.company>
2023-07-17 21:17:16 -0300

    feature: send email by smtp service

--- src/email.rs
+++ src/email.rs
@@ -1,7 +1,8 @@
 use handlebars::Handlebars;
 use lettre::{
-    message::header::ContentType, transport::smtp::authentication::Credentials, AsyncSmtpTransport,
-    AsyncTransport, Message, Tokio1Executor,
+    message::header::ContentType,
+    transport::smtp::authentication::Credentials,
+    AsyncSmtpTransport, AsyncTransport, Message, Tokio1Executor,
 };
 
 use crate::{config::Config, user::User};
@@ -14,7 +15,7 @@ pub struct Email {
 }
 
 impl Email {
-    pub fn new(user: User, url: String, from: String, config: Config) -> Email {
+    pub fn new(user: User, url: String, config: Config) -> Email {
         let from = format!("Codevo <{}>", config.smtp_from.to_owned());
 
         return Email {
@@ -23,5 +24,81 @@ impl Email {
             from,
             config,
         };
+    }
+
+    fn new_transport(
+        &self,
+    ) -> Result<AsyncSmtpTransport<Tokio1Executor>, lettre::transport::smtp::Error> {
+        let credentials = Credentials::new(
+            self.config.smtp_user.to_owned(),
+            self.config.smtp_pass.to_owned(),
+        );
+
+        let transport = AsyncSmtpTransport::<Tokio1Executor>::starttls_relay(
+            &self.config.smtp_host.to_owned(),
+        )?
+        .port(self.config.smtp_port)
+        .credentials(credentials)
+        .build();
+
+        return Ok(transport);
+    }
+
+    fn render_template(&self, template_name: &str) -> Result<String, handlebars::RenderError> {
+        let mut handlebars = Handlebars::new();
+        handlebars.register_template_file(
+            template_name,
+            &format!("./templates/{}.hbs", template_name),
+        )?;
+        handlebars
+            .register_template_file("styles", &format!("./templates/partials/styles.hbs"))?;
+        handlebars.register_template_file("base", "./templates/layouts/base.hbs")?;
+
+        let data = serde_json::json!({
+            "first_name": &self.user.name.split_whitespace().next().unwrap(),
+            "subject": &template_name,
+            "url": &self.url
+        });
+
+        let content_template = handlebars.render(template_name, &data)?;
+        return Ok(content_template);
+    }
+
+    async fn send_email(
+        &self,
+        template_name: &str,
+        subject: &str,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let html_template = self.render_template(template_name)?;
+        let email = Message::builder()
+            .to(
+                format!("{} <{}>", self.user.name.as_str(), self.user.email.as_str())
+                    .parse()
+                    .unwrap(),
+            )
+            .reply_to(self.from.as_str().parse().unwrap())
+            .from(self.from.as_str().parse().unwrap())
+            .subject(subject)
+            .header(ContentType::TEXT_HTML)
+            .body(html_template)?;
+
+        let transport = self.new_transport()?;
+        transport.send(email).await?;
+        return Ok(());
+    }
+
+    pub async fn send_verification_code(&self) -> Result<(), Box<dyn std::error::Error>> {
+        return self
+            .send_email("verification_code", "Your account verification code")
+            .await;
+    }
+
+    pub async fn send_password_reset_token(&self) -> Result<(), Box<dyn std::error::Error>> {
+        return self
+            .send_email(
+                "reset_password",
+                "Your password reset token (valid for only 10 minutes)",
+            )
+            .await;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,33 @@ mod config;
 mod email;
 mod user;
 
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use email::Email;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let config = config::Config::init();
+    let user = user::User {
+        name: String::from("Albert"),
+        email: config.smtp_to.to_owned(),
+    };
+
+    let verification_code = "my_ultra_secure_verification_code";
+    let verification_url = format!("http://localhost:3000/verifyemail/{}", verification_code);
+    let email = Email::new(user, verification_url, config);
+    
+    if let Err(err) = email.send_verification_code().await {
+        eprintln!("Failed to send verification code email: {:?}", err);
+    } else {
+        println!("✅Email verification code sent successfully!");
+    }
+
+    if let Err(err) = email.send_password_reset_token().await {
+        eprintln!("Failed to send password reset token email: {:?}", err);
+    } else {
+        println!("✅Password reset token email sent successfully!");
+    }
+
 }
